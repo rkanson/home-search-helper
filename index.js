ba1b37630b115b58c9976ceebb78a8b889dc5bb2
RJ Kanson <richardkanson@gmail.com>
2021-01-06 17:18:23 -0500

    wip

--- index.js
+++ index.js
@@ -12,30 +12,32 @@ const ObjectsToCsv = require("objects-to-csv");
   const all_houses = [...user_1, ...user_2];
   const houses = Array.from(new Set(all_houses));
 
-  console.log("Authorizing Google Sheets...");
-  const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
-  });
+  // console.log("Authorizing Google Sheets...");
+  // const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
+  // await doc.useServiceAccountAuth({
+  //   client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  //   private_key: process.env.GOOGLE_PRIVATE_KEY,
+  // });
 
-  console.log("Getting sheet...");
-  await doc.loadInfo();
-  const sheet = doc.sheetsByIndex[0];
-  const combSheet = doc.sheetsByIndex[1];
+  // console.log("Getting sheet...");
+  // await doc.loadInfo();
+  // const sheet = doc.sheetsByIndex[0];
+  // // const combSheet = doc.sheetsByIndex[1];
 
-  console.log("Clearing old rows...");
-  const rows = await sheet.getRows();
-  await rows.map((row, index) => {
-    if (index != 0) {
-      row.delete();
-    }
-  });
+  // // console.log("Clearing old rows...");
+  // // const rows = await sheet.getRows();
+  // // await rows.map((row, index) => {
+  // //   if (index != 0) {
+  // //     setTimeout(() => {
+  // //       row.delete();
+  // //     }, 1000);
+  // //   }
+  // // });
 
-  console.log("Adding to Google Sheets...");
-  await sheet.addRows(houses);
-  // await combSheet.addRows(user_1);
-  // await combSheet.addRows(user_2);
+  // console.log("Adding to Google Sheets...");
+  // await sheet.addRows(houses);
+  // // await combSheet.addRows(user_1);
+  // // await combSheet.addRows(user_2);
 
   console.log("Complete! Outputting to CSV.");
   const csv = new ObjectsToCsv(houses);
